76cd990f4751ad6439505d5e49690355a8bb9419
yanfalcao <33384608+yanfalcao@users.noreply.github.com>
2018-06-26 19:59:12 -0300

    Add files via upload

---
+++ 1038.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	double a, b, c, delta, r1, r2;
+	
+	scanf("%lf %lf %lf", &a, &b, &c);
+	
+	if(a==0) {
+		
+		printf("Impossivel calcular\n");
+		return 0;
+	}
+	
+	delta = (b*b) - (1*4*a*c);
+		
+	if(delta<0) {
+		
+		printf("Impossivel calcular\n");
+		return 0;
+	}
+	
+	delta = sqrt(delta);
+	r1 = (-b+delta) / (2*a);
+	r2 = (-b-delta) / (2*a);
+	
+	printf("R1 = %.5lf\nR2 = %.5lf\n",r1 ,r2);
+}
