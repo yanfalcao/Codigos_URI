88fb70224e06339cdd203093c55e8a2ad59ccbed
yanfalcao <33384608+yanfalcao@users.noreply.github.com>
2018-06-27 18:31:33 -0300

    Update and rename 1019.c to Conversao_de_Tempo.c

--- Conversao_de_Tempo.c
+++ Conversao_de_Tempo.c
@@ -1,4 +1,4 @@
-/*QUESTAO
+/*1019
 	Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica,
 	e informe-o expresso no formato horas:minutos:segundos.
 */
