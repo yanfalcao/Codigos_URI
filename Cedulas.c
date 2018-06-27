9f8d239a67ec6e61ec4acd5d1f26f76b31c35ff4
yanfalcao <33384608+yanfalcao@users.noreply.github.com>
2018-06-27 18:30:50 -0300

    Update and rename 1018.c to Cedulas.c

--- Cedulas.c
+++ Cedulas.c
@@ -1,4 +1,4 @@
-/*QUESTAO
+/*1018
 	Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) no qual o valor pode ser decomposto.
 	As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1.
 	A seguir mostre o valor lido e a relação de notas necessárias.
