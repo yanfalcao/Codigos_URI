7794f7ef27e764d13930b9675ac9d72ae185a17c
yanfalcao <33384608+yanfalcao@users.noreply.github.com>
2018-06-27 18:39:51 -0300

    Update and rename 1041.c to Coordenadas_de_um_Ponto.c

--- Coordenadas_de_um_Ponto.c
+++ Coordenadas_de_um_Ponto.c
@@ -1,10 +1,10 @@
-/*
+/*1041
 	Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. 
-	A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+	A seguir, determine qual o quadrante ao qual pertence o ponto, ou se estÃ¡ sobre um dos eixos cartesianos ou na origem (x = y = 0).
 	
-	Se o ponto estiver na origem, escreva a mensagem “Origem”.
+	Se o ponto estiver na origem, escreva a mensagem â€œOrigemâ€.
 
-	Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+	Se o ponto estiver sobre um dos eixos escreva â€œEixo Xâ€ ou â€œEixo Yâ€, conforme for a situaÃ§Ã£o.
 */
 #include<stdio.h>
 
