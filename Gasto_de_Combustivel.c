d681384a9ddcca0324a5b38cf92638511a3561f7
yanfalcao <33384608+yanfalcao@users.noreply.github.com>
2018-06-27 18:29:45 -0300

    Update and rename 1017.c to Gasto_de_Combustivel.c

--- Gasto_de_Combustivel.c
+++ Gasto_de_Combustivel.c
@@ -1,4 +1,4 @@
-/*QUESTAO
+/*1017
 	Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem,
 	ao utilizar um automóvel que faz 12 KM/L.Para isso, ele gostaria que você o auxiliasse através de um simples programa.
 	Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h).
