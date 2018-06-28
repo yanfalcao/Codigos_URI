6406e1bba3866f7f92a8638ce54b07eaf6c841e6
yanfalcao <33384608+yanfalcao@users.noreply.github.com>
2018-06-28 11:10:50 -0300

    Rename Intervalo_2.cpp to Intervalo_2.c

--- Intervalo_2.c
+++ Intervalo_2.c
@@ -1,18 +1,18 @@
 /*1072	
 
-	Leia um valor inteiro N. Este valor ser· a quantidade de valores inteiros X que ser„o lidos em seguida.
-	Mostre quantos destes valores X est„o dentro do intervalo [10,20] e quantos est„o fora do intervalo,
-	mostrando essas informaÁıes.
+	Leia um valor inteiro N. Este valor ser√° a quantidade de valores inteiros X que ser√£o lidos em seguida.
+	Mostre quantos destes valores X est√£o dentro do intervalo [10,20] e quantos est√£o fora do intervalo,
+	mostrando essas informa√ß√µes.
 	
 	Entrada
 
-	A primeira linha da entrada contÈm um valor inteiro N (N < 10000), 
-	que indica o n˙mero de casos de teste.
-	Cada caso de teste a seguir È um valor inteiro X (-107 < X <107).
+	A primeira linha da entrada cont√©m um valor inteiro N (N < 10000), 
+	que indica o n√∫mero de casos de teste.
+	Cada caso de teste a seguir √© um valor inteiro X (-107 < X <107).
  
-	SaÌda
+	Sa√≠da
 
-	Para cada caso, imprima quantos n˙meros est„o dentro (in) e quantos valores est„o fora (out) do intervalo.
+	Para cada caso, imprima quantos n√∫meros est√£o dentro (in) e quantos valores est√£o fora (out) do intervalo.
 
 */
 #include<stdio.h>
